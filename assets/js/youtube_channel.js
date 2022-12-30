9fb23d260a6d287afb5ce83daf42f1b067550994
Reka Prihatanto <rey.barrolz@gmail.com>
2022-12-30 14:22:52 +0700

    update link yutub warga

--- assets/js/youtube_channel.js
+++ assets/js/youtube_channel.js
@@ -1,62 +1,72 @@
-// sementara didisable dulu
+var vidio_list = [];
 
-// // daftar channel ID warga jiwa gelap
-// const channel_id_youtube_list = {
-//   Reka: "UCeF7_Y1UhBCkZ56pb-Z-ASA",
-//   Lothricel: "UCxrW-_FCRUl1KMmMSDVXH3g",
-//   Alan: "UCYX3GSPUSM3SRUlBY4axrjA",
-// };
+// daftar channel ID warga jiwa gelap
+const channel_id_youtube_list = {
+  Reka: "UCeF7_Y1UhBCkZ56pb-Z-ASA",
+  Lothricel: "UCxrW-_FCRUl1KMmMSDVXH3g",
+  Alan: "UCYX3GSPUSM3SRUlBY4axrjA",
+};
 
-// $.each(channel_id_youtube_list, function (key_channel, value_channel) {
-//   $("#user_list").append(
-//     "<h1>" +
-//       key_channel +
-//       '</h1> <hr> <div class="d-flex flex-wrap" id="yutub_vidio_list-' +
-//       value_channel +
-//       '"></div>'
-//   );
+$.each(channel_id_youtube_list, function (key_channel, value_channel)  {
+  $("#user_list").append(
+    "<h1>" +
+      key_channel +
+      '</h1> <hr> <div class="d-flex flex-wrap" id="yutub_vidio_list-' +
+      value_channel +
+      '"></div>'
+  );
 
-//   form_data = {};
-//   $.ajax({
-//     url:
-//       "https://api.rss2json.com/v1/api.json?rss_url=https://www.youtube.com/feeds/videos.xml?channel_id=" +
-//       value_channel,
-//     type: "GET",
-//     data: form_data,
-//     error: function (err) {
-//       $("#loading").hide();
-//       swal("Oops, something went wrong!", null, "error");
-//     },
-//     success: function (ok) {
-//       var no = 1;
-//       $.each(ok.items, function (key, value) {
-//         console.log(value);
-//         if (no <= 6) {
-//           const tooltipTriggerList = document.querySelectorAll(
-//             '[data-bs-toggle="tooltip"]'
-//           );
-//           const tooltipList = [...tooltipTriggerList].map(
-//             (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
-//           );
+  form_data = {};
+  $.ajax({
+    url:
+      "https://api.rss2json.com/v1/api.json?rss_url=https://www.youtube.com/feeds/videos.xml?channel_id=" +
+      value_channel,
+    type: "GET",
+    data: form_data,
+    error: function (err) {
+      $("#loading").hide();
+      swal("Oops, something went wrong!", null, "error");
+    },
+    success: function (ok) {
+      var no = 1;
+      $.each(ok.items, function (key, value) {
+        // console.log(value);
 
-//           $("#yutub_vidio_list-" + value_channel).append(
-//             $("#template_youtube_channel")
-//               .html()
-//               .replace(/%tumbnail%/g, value.thumbnail)
-//               .replace(/%judul%/g, value.title)
-//               .replace(/%url%/g, value.link)
-//               .replace(
-//                 /%published%/g,
-//                 moment(value.pubDate, "YYYYMMDD").fromNow()
-//               )
-//               .replace(/%tgl_tooltip%/g, value.pubDate)
-//           );
-//         }
-//         no++;
-//       });
-//     },
-//   });
-// });
+        // batasi sampai 6 konten saja
+        if (no <= 6) {
+
+          // script untuk aktivasi tooltip
+          const tooltipTriggerList = document.querySelectorAll( '[data-bs-toggle="tooltip"]' );
+          const tooltipList = [...tooltipTriggerList].map( (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)  );
+
+          $("#yutub_vidio_list-" + value_channel).append(
+            $("#template_youtube_channel")
+              .html()
+              .replace(/%tumbnail%/g, value.thumbnail)
+              .replace(/%judul%/g, value.title)
+              .replace(/%url%/g, value.link)
+              .replace(
+                /%published%/g,
+                moment(value.pubDate, "YYYYMMDD").fromNow()
+              )
+              .replace(/%tgl_tooltip%/g, value.pubDate)
+          );
+
+        }
+        no++;
+      });
+    },
+  });
+});
+// end of fetch youtube channel data
+
+
+
+
+
+
+
+
 
 form_data = {};
 $.ajax({
