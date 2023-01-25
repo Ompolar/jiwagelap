323bf24ffa5d45f8dc4fc0278eeaa6ea1d8b8b7a
Reka Prihatanto <rey.barrolz@gmail.com>
2023-01-25 19:19:07 +0700

    fix load js

--- website-laravel/resources/views/layouts/app.blade.php
+++ website-laravel/resources/views/layouts/app.blade.php
@@ -53,14 +53,9 @@
 
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 
   <!-- Bootstrap JavaScript -->
-  <script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-    crossorigin="anonymous"
-  ></script>
+
   <script async src="https://platform.twitter.com/widgets.js" charset="utf-8" ></script>
 
   <script src="/my_assets/js/app.js?v=0.8"></script>
--- website-laravel/resources/views/layouts/components/head.blade.php
+++ website-laravel/resources/views/layouts/components/head.blade.php
@@ -3,17 +3,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <!-- Bootstrap CSS -->
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-      crossorigin="anonymous"
-    />
     <link rel="stylesheet" href="./my_assets/css/font-awesome.min.css" />
 
     <!-- custom css -->
     <script src="https://unpkg.com/scrollreveal"></script>
 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 
 
 
