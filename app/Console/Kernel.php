8dadb89444c2f8a11b592df66e2bc7edbd535d02
Reka Prihatanto <rey.barrolz@gmail.com>
2023-08-12 16:44:54 +0700

    update scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('jiwagelap:post_youtube_notification')->everyFiveMinutes();
         // $schedule->command('inspire')->hourly();
     }
 
