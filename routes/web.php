cb7616e6660a58db7839a1beaae4ca0d0028f119
Reka Prihatanto <rey.barrolz@gmail.com>
2023-08-19 17:35:56 +0700

    update route agar tidak login ke default

--- routes/web.php
+++ routes/web.php
@@ -21,17 +21,26 @@ Route::get('/', [HomeController::class, 'welcome']);
 
 Auth::routes(['register' => false]);
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/youtube_channel', [ChannelController::class, 'index'])->name('youtube_channel');
 
 
-Route::get('/bot', [BotController::class, 'index'])->name('bot');
-Route::post('/bot_insert', [BotController::class, 'insert'])->name('bot.insert');
-Route::post('/bot_delete', [BotController::class, 'delete'])->name('bot.delete');
+
+Route::middleware(['auth'])->group(function () {
+
+    // halaman utama setelah login
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    // kelola bot, masukkan ke dalam box sebelah kiri template
+    Route::get('/bot', [BotController::class, 'index'])->name('bot');
+    Route::post('/bot_insert', [BotController::class, 'insert'])->name('bot.insert');
+    Route::post('/bot_delete', [BotController::class, 'delete'])->name('bot.delete');
 
 
+    // kelola notifikasi youtube yg bisa dikirim ke server sebagai webhook
+    Route::get('/yt_notification', [YoutubeNotificationController::class, 'index'])->name('yt_notification');
+    Route::post('/yt_notification_insert', [YoutubeNotificationController::class, 'insert'])->name('yt_notification.insert');
+    Route::post('/yt_notification_delete', [YoutubeNotificationController::class, 'delete'])->name('yt_notification.delete');
+    Route::post('/yt_notification_send_last', [YoutubeNotificationController::class, 'send'])->name('yt_notification.send_last');
+});
 
-Route::get('/yt_notification', [YoutubeNotificationController::class, 'index'])->name('yt_notification');
-Route::post('/yt_notification_insert', [YoutubeNotificationController::class, 'insert'])->name('yt_notification.insert');
-Route::post('/yt_notification_delete', [YoutubeNotificationController::class, 'delete'])->name('yt_notification.delete');
-Route::post('/yt_notification_send_last', [YoutubeNotificationController::class, 'send'])->name('yt_notification.send_last');
+
